d978db7438338707cbf10b4532d7bd091c231aba
Gilliaard Damme <dammeg@kpnmail.nl>
2019-06-14 06:28:08 +0200

    Gebruiker kunnen volgen

---
+++ src/Controller/FollowController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+
+class FollowController extends AbstractController
+{
+    /**
+     * @Route("/users/{user}/follow/{follow}", name="user_follow")
+     */
+    public function follow(User $user, User $follow)
+    {
+
+        $user->addFollower($follow);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->json(true);
+    }
+
+    /**
+     * @Route("/users/{user}/follow/{follow}", name="follow", name="user_unfollow")
+     */
+    public function unfollow(User $user, User $follow)
+    {
+        $user->removeFollower($follow);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->json(true);
+    }
+}
